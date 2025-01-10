2b45bdaf482454373da464eb4e342ed8756b9d08
PavloMarkovskiy <pavlo.markovskiy@brazinomarketing.com>
2025-01-10 02:40:37 +0000

    Bag with scriptable object repaired

--- Assets/Scripts/Game/Entities/Player/Data/PlayerDataManager.cs
+++ Assets/Scripts/Game/Entities/Player/Data/PlayerDataManager.cs
@@ -1,12 +1,14 @@
-﻿public class PlayerDataManager
+﻿using System.Collections.Generic;
+
+public class PlayerDataManager
 {
     public CharacterData ConfigureCharacterData(CharacterConfig basicCharacterConfig, CharacterData oldCharacterData)
     {
         CharacterData characterData = oldCharacterData;
 
         characterData.CharacterPrefab = basicCharacterConfig.CharacterPrefab;
-        characterData.Skills = basicCharacterConfig.Skills;
-        characterData.Parameters = basicCharacterConfig.Parameters;
+        characterData.Skills = new List<SkillTypes>(basicCharacterConfig.Skills);
+        characterData.Parameters = new Dictionary<PlayerParameterTypes, float>(basicCharacterConfig.Parameters);
 
         return characterData;
 
